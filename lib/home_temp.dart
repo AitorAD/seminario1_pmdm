0b18e09a9b40fd0b435b129eefeb3d59bb474726
Aitor Agullo Duque <dam_ada@informatica.mz>
2024-10-03 13:04:59 +0200

    Seminario 1

--- lib/home_temp.dart
+++ lib/home_temp.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Componentes Temp'),
-      ),
-      body: ListView(),
+    return Scaffold(appBar: AppBar(title: Text('Componentes')), body: _lista());
+  }
+
+  Widget _lista() {
+    return ListView(
+      children: _listaItems(),
     );
+  }
+
+  List<Widget> _listaItems() {
+    return [
+      ListTile(title: Text('Hola Mundo')),
+      Divider(),
+      ListTile(title: Text('Hola Mundo')),
+      Divider()
+    ];
   }
 }
