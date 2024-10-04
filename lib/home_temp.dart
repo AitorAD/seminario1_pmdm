98bcadec4f525dbc531c9af1956b64be37cd63f2
Aitor Agullo Duque <dam_ada@informatica.mz>
2024-10-04 09:00:18 +0200

    Seminario 1

--- lib/home_temp.dart
+++ lib/home_temp.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:seminario_1/providers/menu_providers.dart';
 
-class HomePageTemp extends StatelessWidget {
-  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text('Componentes')), body: _lista());
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      //Argumento opcional, y será la información que tendrá por defecto mientras
+      //no se haya resuelto la promesa
+      // initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('builder');
+        print(snapshot.data);
+        //Crearemos el ListView
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-      Divider()
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    // Recorreremos las opciones
+    // Anteriormente esto lo hicimos con un for extendido for in o con un .map
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+    return opciones;
   }
 }
